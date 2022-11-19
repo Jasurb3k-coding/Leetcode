c57aa022777b993adb106bb2b0341026d402f488
Jasurb3k-coding <105157721+Jasurb3k-coding@users.noreply.github.com>
2022-11-19 20:34:33 +0100

    2181

---
+++ medium/2181.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var mergeNodes = function(head) {
+    let left = head
+    let right = head.next
+    let tmp_sum = 0
+    while (right){
+        tmp_sum += right.val
+        if (right.val == 0){
+            left = left.next
+            left.val = tmp_sum
+            tmp_sum = 0
+        }
+        right = right.next
+    }
+    left.next = null
+    // console.log(res)
+    return head.next
+};
